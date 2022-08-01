cc28a1c56f81b47f8d457edb7bf500a05235e585
DipaleeDukare <110393158+DipaleeDukare@users.noreply.github.com>
2022-08-01 18:45:55 +0530

    Update Demo.java
    by.dipalee

--- GitHub/src/aug1/Demo.java
+++ GitHub/src/aug1/Demo.java
@@ -3,12 +3,15 @@ package aug1;
 public class Demo {
 	public void m1()
 	{
+	System.out.println("hub2");
+
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Git hub");
 		System.out.println("hub1");
 		System.out.println("hub2");
+		
 	}
 
 }
